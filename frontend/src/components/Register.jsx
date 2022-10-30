0ce635864dc22cbf3dc207d934f72864eb9f27cf
Nita Constantin <nita.constantin880722@yahoo.com>
2022-10-30 13:09:37 +0200

    248. Create Router Design Login and Register Page Part 2

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,9 +1,71 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      <h1>This is Register Page</h1>
+    <div className="register">
+      <div className="card">
+        <div className="card-header">
+          <h3>Register</h3>
+        </div>
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label htmlFor="username">User Name</label>
+              <input
+                type="text"
+                id="username"
+                className="form-control"
+                placeholder="User Name"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                placeholder="E-mail"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                className="form-control"
+                placeholder="Password"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                id="confirmPassword"
+                className="form-control"
+                placeholder="Confirm Password"
+              />
+            </div>
+            <div className="form-group">
+              <div className="file-image">
+                <div className="image"></div>
+                <div className="file">
+                  <label htmlFor="image">Select image</label>
+                  <input type="file" id="image" className="form-control" />
+                </div>
+              </div>
+            </div>
+            <div className="form-group">
+              <input type="submit" value="Register" className="btn" />
+            </div>
+            <div className="form-group">
+              <span>
+                <Link to="/messenger/login">Login Your Account</Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
